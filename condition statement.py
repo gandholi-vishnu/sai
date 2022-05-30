0b17e925d2857a7c2055dcda7e7bb33dc0dcd2a6
gandholi-vishnu <106440768+gandholi-vishnu@users.noreply.github.com>
2022-05-30 17:57:34 +0530

    implimented condition statements

---
+++ condition statement.py
@@ -0,0 +1,11 @@
+input=100
+if input < 100:
+  print("input value is less than 100")
+elif input == 100:
+  print("input is equal to 100")
+elif input >100 and input<150:
+  print("input value is between 100 to 150")
+elif input > 500:
+  print("input value is greater than 500")
+else:
+  print("this is your input value: ",input,)
