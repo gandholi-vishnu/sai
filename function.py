222c7c9535a2ad0d057a3416f2420d7c6aeb4087
gandholi-vishnu <106440768+gandholi-vishnu@users.noreply.github.com>
2022-05-31 19:14:35 +0530

    Update function.py

--- function.py
+++ function.py
@@ -6,3 +6,30 @@ def my_function(list_of_arrays):
    print("hihi")  
 array_num=[1,2,3,4,5,6,7,8,9,10]
 print(my_function(array_num))
+
+
+
+
+
+
+
+
+
+
+
+
+def my_function(list_of_arrays):
+  numf=0
+  for x in array_num:
+    numf=x + numf
+  return numf  
+array_num=[2,4,6]
+print(my_function(array_num))
+def matrix_funtion(list_of_matrix):
+  numf=0
+  for x in matrix:
+    numf=my_function(x) + numf
+  return numf
+matrix=[[2,4,6],[9, 5, 7],[3, 8, 1]]
+print(matrix_funtion(matrix)) 
+
