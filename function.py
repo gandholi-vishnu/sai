c309194f676555cd42006b00fa646832eb3159d8
gandholi-vishnu <106440768+gandholi-vishnu@users.noreply.github.com>
2022-05-31 17:28:26 +0530

    implimented new function
    learnt indentation , sintax , parameters ,

---
+++ function.py
@@ -0,0 +1,8 @@
+def my_function(list_of_arrays):
+   numf=0
+   for x in array_num:
+     numf=x + numf
+   return numf
+   print("hihi")  
+array_num=[1,2,3,4,5,6,7,8,9,10]
+print(my_function(array_num))
